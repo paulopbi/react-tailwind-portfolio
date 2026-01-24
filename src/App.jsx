07a00462fc09bc3a1116d1337d45354ffd274ae3
Paulo Victor <paulovictordev16@gmail.com>
2026-01-24 17:16:41 -0300

    feat(Projects): add projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from '@/layout/Navbar'
 import About from '@/sections/About'
 import Hero from '@/sections/Hero'
+import Projects from './sections/Projects'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Projects />
       </main>
     </div>
   )
---
+++ src/sections/Projects.jsx
@@ -0,0 +1,142 @@
+import AnimatedBorderButton from '@/components/AnimatedBorderButton'
+import { ArrowUpRight, Github } from 'lucide-react'
+
+const projects = [
+  {
+    title: 'Fintech Dashborard',
+    description: 'teste',
+    image: '/projects/project1.png',
+    tags: ['React', 'Javascript', 'TailwindCSS'],
+    link: '#',
+    github: '#',
+  },
+  {
+    title: 'Fintech Dashborard',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis repellendus vero, commodi temporibus voluptas molestias qui rem optio voluptatum dicta totam odit repellat harum neque voluptatem repudiandae esse aperiam quod.',
+    image: '/projects/project2.png',
+    tags: ['Next.js', 'Typescript', 'CSS'],
+    link: '#',
+    github: '#',
+  },
+  {
+    title: 'AI Writing Assistant',
+    description:
+      'An intelligent writing tool powered by GPT-4, helping users create better content faster.',
+    image: '/projects/project2.png',
+    tags: ['React', 'OpenAI', 'Python', 'FastAPI'],
+    link: '#',
+    github: '#',
+  },
+  {
+    title: 'Project Management Tool',
+    description:
+      'A collaborative workspace for teams with real-time updates, task tracking, and integrations.',
+    image: '/projects/project1.png',
+    tags: ['Next.js', 'Socket.io', 'MongoDB', 'Redis'],
+    link: '#',
+    github: '#',
+  },
+]
+
+function Projects() {
+  return (
+    <section id="projetos" className="py-32 relative overflow-hidden">
+      {/* bg glows */}
+      <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
+      <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+      <div className="container mx-auto px-6 relative z-10">
+        <div className="text-center mx-auto max-w-3xl mb-16">
+          <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">
+            Featured Work
+          </span>
+          <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
+            Projects that
+            <span className="font-serif italic font-normal text-white">
+              {' '}
+              make inpact.
+            </span>
+          </h2>
+          <p className="text-muted-foreground animate-fade-in animation-delay-200">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
+            sint voluptatem temporibus architecto unde est perferendis corrupti
+            rem doloremque praesentium reprehenderit illo aspernatur minus
+            aliquam, molestiae, dolore incidunt inventore ducimus.
+          </p>
+        </div>
+
+        {/* projects grid */}
+        <div className="grid md:grid-cols-2 gap-8">
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              style={{ animationDelay: `${index + 1 * 100}ms` }}
+              className="group glass rounded-2xl overflow-hidden animate-fade-in md:row-span-1"
+            >
+              <div className="relative overflow-hidden aspect-video">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
+                />
+
+                <div className="absolute inset-0 bg-linear-to-t from-card via-card/50 to-transparent backdrop-opacity-60" />
+
+                {/* overlay links */}
+                <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all"
+                  >
+                    <ArrowUpRight className="w-5 h-5" />
+                  </a>
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all"
+                  >
+                    <Github className="w-5 h-5" />
+                  </a>
+                </div>
+              </div>
+
+              {/* content */}
+              <div className="p-6 space-y-4">
+                <div className="flex items-start justify-between">
+                  <h3 className="text-xl font-semibold group-hover:text-primary transition-colors">
+                    {project.title}
+                  </h3>
+                  <ArrowUpRight className="w-5 h-5 text-muted-foreground group-hover:text-primary group-hover:translate-x-1 group-hover:-translate-y-1  transition-all" />
+                </div>
+                <p className="text-muted-foreground text-sm">
+                  {project.description}
+                </p>
+                <div className="flex flex-wrap gap-2">
+                  {project.tags.map((tag, tagIndex) => (
+                    <span
+                      key={tagIndex}
+                      className="px-4 py-1.5 rounded-full bg-surface text-xs font-medium border border-border/50 text-muted-foreground hover:border-primary/50 hover:text-primary transition-all duration-300"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* view all */}
+        <div className="text-center mt-12 animate-fade-in animation-delay-500">
+          <AnimatedBorderButton>
+            Todos os projetos
+            <ArrowUpRight className="w-5 h-5" />
+          </AnimatedBorderButton>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
