a5f56b00b754ba015d06dee33e066af133ef605e
Paulo Victor <paulovictordev16@gmail.com>
2026-01-25 20:12:13 -0300

    feat(Navbar): remove navbar links for sections no longer in use

--- src/constants/navbarLinks.js
+++
@@ -1,6 +0,0 @@
-export const navbarLinks = [
-  { href: '#sobre', label: 'Sobre' },
-  { href: '#projetos', label: 'Projetos' },
-  { href: '#experiencia', label: 'Experiência' },
-  { href: '#testmonials', label: 'Testimonials' },
-]
